ff63b77ee0d365fbd64748c5fbbd483bb6ccf5ef
reidmtu <115564106+reidmtu@users.noreply.github.com>
2023-04-27 12:33:17 +0100

    Comments removed
    All comments removed

--- test.py
+++ test.py
@@ -1,14 +1,6 @@
 import psutil
 import PySimpleGUI as GUI
 
-# Outside the while loop to use print statement
-#  disk_io_counters = psutil.disk_io_counters()
-#  print(disk_io_counters)
-#  network = psutil.net_if_addrs()
-#  print(tcp_networks)
-#  network_pernic = psutil.net_io_counters(pernic=True)
-#  print(network_pernic)
-
 layout = [[GUI.Text('CPU Usage:'), GUI.Text('', size=(20, 1), key='-cpu_usage-')],
           [GUI.Text('Total CPU Cores:'), GUI.Text('', size=(20, 1), key='-total_cpu-')],
           [GUI.Text('CPU Cores:'), GUI.Text('', size=(20, 1), key='-cpu_cores-')],
@@ -20,25 +12,21 @@ layout = [[GUI.Text('CPU Usage:'), GUI.Text('', size=(20, 1), key='-cpu_usage-')
          [GUI.Button('Exit')]
 
 window = GUI.Window('System Monitor', layout, size=(725, 250))
-#  size=(725, 200) makes the window big to allow the 'ram_info'
 
 while True:
     event, values = window.read(timeout=200)
 
     if event == GUI.WINDOW_CLOSED or event == 'Exit':
         break
-    # device information that changes
-    cpu_usage = psutil.cpu_percent()  # cpu usage as a percentage
-    ram_usage = psutil.virtual_memory().percent  # ram usage as a percentage
-    # device information that does not change
-    total_cpu = psutil.cpu_count()  # cpu core of cores including logical cores
-    cpu_cores = psutil.cpu_count(logical=False)  # cpu cores only
-    ram_info = psutil.virtual_memory()  # the amount of memory in the system
+    cpu_usage = psutil.cpu_percent()
+    ram_usage = psutil.virtual_memory().percent
+    total_cpu = psutil.cpu_count()
+    cpu_cores = psutil.cpu_count(logical=False)
+    ram_info = psutil.virtual_memory()
     user_info = psutil.users()
-    partition_info = psutil.disk_partitions()  # gets information on the disk partitions of the system
-    disk_usage = psutil.disk_usage('/' or "C:")  # "C:" for Windows, '/' for Linux
+    partition_info = psutil.disk_partitions()
+    disk_usage = psutil.disk_usage('/' or "C:")
 
-    # information is updated and sent to the GUI window
     window['-cpu_usage-'].update(f'{cpu_usage}%')
     window['-total_cpu-'].update(f'{total_cpu}')
     window['-cpu_cores-'].update(f'{cpu_cores}')
